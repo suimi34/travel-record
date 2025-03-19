5432f64480234f30c76f3046f92f7e13b324ccfd
Susumu Arimoto <susumu2934@gmail.com>
2025-03-19 22:17:37 +0900

    add destinations

--- src/destination-list/index.tsx
+++ src/destination-list/index.tsx
@@ -9,10 +9,16 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { MapPin, Sun, Umbrella, Mountain } from 'lucide-react'
 
+const SEOUL_IMAGE = "/seoul_korea.jpg";
 const KYUFUN_IMAGE = "/kyufun_taiwan.jpg";
+const MANILA_IMAGE = "/manila_philippines.jpg";
+const VANCOUVER_IMAGE = "/vancouver_canada.jpg";
 
 const destinations = [
-  { id: 1, name: "Jiufen, Taiwan", type: "beach", weather: "tropical", image: KYUFUN_IMAGE },
+  { id: 1, name: "Seoul, Korea", type: "city", weather: "tropical", image: SEOUL_IMAGE },
+  { id: 2, name: "Jiufen, Taiwan", type: "beach", weather: "tropical", image: KYUFUN_IMAGE },
+  { id: 3, name: "Manila, Philippines", type: "beach", weather: "tropical", image: MANILA_IMAGE },
+  { id: 4, name: "Vancouver, Canada", type: "city", weather: "cold", image: VANCOUVER_IMAGE },
 ]
 
 export default function TravelFinder() {
