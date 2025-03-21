daf94498ac7fd688baef19f660d84ff896c32117
Susumu Arimoto <susumu2934@gmail.com>
2025-03-21 15:19:43 +0900

    add. gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
+      <GoogleTagManager gtmId="GTM-PL4H9LVQ" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
