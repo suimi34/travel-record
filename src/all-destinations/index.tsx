b235d49d4ea2a67ff7a55255c9239eaf3c01ef3c
Susumu Arimoto <susumu2934@gmail.com>
2025-03-29 12:57:00 +0900

    add. north america cities

--- src/all-destinations/index.tsx
+++ src/all-destinations/index.tsx
@@ -2,10 +2,20 @@ const SEOUL_IMAGE = "/seoul_korea.jpg";
 const KYUFUN_IMAGE = "/kyufun_taiwan.jpg";
 const MANILA_IMAGE = "/manila_philippines.jpg";
 const VANCOUVER_IMAGE = "/vancouver_canada.jpg";
+const SEATTLE_IMAGE = "/seattle_us.jpg";
+const PORTLAND_IMAGE = "/portland_us.jpg";
+const SAN_FRANCISCO_IMAGE = "/san_francisco_us.jpg";
+const SAN_JOSE_IMAGE = "/san_jose_us.jpg";
+const LOS_ANGELS_IMAGE = "/los_angels_us.jpg";
 
 export const ALL_DESTINATIONS = [
-  { id: 1, name: "Seoul, Korea", type: "city", weather: "tropical", image: SEOUL_IMAGE },
+  { id: 1, name: "Seoul, Korea", type: "city", weather: "varied", image: SEOUL_IMAGE },
   { id: 2, name: "Jiufen, Taiwan", type: "beach", weather: "tropical", image: KYUFUN_IMAGE },
   { id: 3, name: "Manila, Philippines", type: "beach", weather: "tropical", image: MANILA_IMAGE },
   { id: 4, name: "Vancouver, Canada", type: "city", weather: "cold", image: VANCOUVER_IMAGE },
+  { id: 5, name: "Seattle, United States", type: "city", weather: "cold", image: SEATTLE_IMAGE },
+  { id: 6, name: "Portland, United States", type: "city", weather: "cold", image: PORTLAND_IMAGE },
+  { id: 7, name: "San Francisco, United States", type: "city", weather: "mediterranean", image: SAN_FRANCISCO_IMAGE },
+  { id: 8, name: "San Jose, United States", type: "city", weather: "mediterranean", image: SAN_JOSE_IMAGE },
+  { id: 9, name: "Los Angels, United States", type: "city", weather: "mediterranean", image: LOS_ANGELS_IMAGE },
 ]
