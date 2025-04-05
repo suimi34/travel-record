ba3f30035acaeb157dee8eb80ddf57459f8972b6
Susumu Arimoto <susumu2934@gmail.com>
2025-04-05 21:58:13 +0900

    use. lambda for s3 pre signed url

--- src/destination/read-more-wrapper/index.tsx
+++ src/destination/read-more-wrapper/index.tsx
@@ -3,7 +3,6 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
 import Image from "next/image";
-import { getSignedUrlAction } from '@/app/actions'
 
 const ReadMore = dynamic(() => import("../read-more"), { ssr: false });
 
@@ -11,12 +10,33 @@ export default function ReadMoreWrapper(props: { showReadMore: boolean; imageKey
   const [imageUrls, setImageUrls] = useState<string[]>([]);
   const [availableImageKeys, setAvailableImageKeys] = useState<string[]>(props.imageKeys);
 
+  // Fetch signed URL from Lambda function because Next.js static export does not support server actions
+  const getSignedUrlLambda = async (key: string) => {
+    try {
+      const response = await fetch(`${process.env.S3_GET_SIGNED_URL_API_END_POINT}/getSignedUrl?key=${key}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      return data.url;
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw error;
+    };
+  }
+
   const appendImageUrls = async () => {
-    const keys = availableImageKeys.slice(0, 9);
-    const urls = await Promise.all(keys.map((key) => getSignedUrlAction(key)));
+    const appendingKeys = availableImageKeys.slice(0, 9);
+    const urls = await Promise.all(appendingKeys.map((key) => getSignedUrlLambda(key)));
     const newImageUrls = [...imageUrls, ...urls];
     setImageUrls(newImageUrls);
-    setAvailableImageKeys(availableImageKeys.filter((key) => !availableImageKeys.includes(key)));
+    const remainingImageKeys = availableImageKeys.filter((key) => !appendingKeys.includes(key))
+    setAvailableImageKeys(remainingImageKeys);
   }
   const handleClick = () => {
     appendImageUrls();
